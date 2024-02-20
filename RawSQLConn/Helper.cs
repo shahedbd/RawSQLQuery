7a8f83990dcad310814887a0daff3f682742c854
R M Shahidul Islam <shahedbddev@gmail.com>
2024-02-20 14:28:08 +0600

    + Project code added

---
+++ RawSQLConn/Helper.cs
@@ -0,0 +1,53 @@
+﻿using RawSQLConn.Model;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace RawSQLConn
+{
+    public static class Helper
+    {
+        public static string connectionString = "Server=DESKTOP-99Q87I2\\MSSQLSERVER2017;Database=BusinessERP;User ID=sa;Password=dev123456;MultipleActiveResultSets=true";
+
+        public static List<Department> GetAllDepartment(SqlConnection connection)
+        {
+            try
+            {
+                List<Department> lsitDepartment = new();
+                string query = "SELECT * FROM Department";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader _SqlDataReader = command.ExecuteReader())
+                    {
+                        lsitDepartment = DataReaderMapToList<Department>(_SqlDataReader);
+                    }
+                }
+
+                return lsitDepartment;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static List<T> DataReaderMapToList<T>(IDataReader _IDataReader)
+        {
+            List<T> list = new List<T>();
+            T _Objetc = default(T);
+            while (_IDataReader.Read())
+            {
+                _Objetc = Activator.CreateInstance<T>();
+                foreach (PropertyInfo _PropertyInfo in _Objetc.GetType().GetProperties())
+                {
+                    if (!object.Equals(_IDataReader[_PropertyInfo.Name], DBNull.Value))
+                    {
+                        _PropertyInfo.SetValue(_Objetc, _IDataReader[_PropertyInfo.Name], null);
+                    }
+                }
+                list.Add(_Objetc);
+            }
+            return list;
+        }
+    }
+}
---
+++ RawSQLConn/Program.cs
@@ -0,0 +1,24 @@
+﻿using RawSQLConn;
+using System.Data.SqlClient;
+
+
+using (SqlConnection connection = new(Helper.connectionString))
+{
+    try
+    {
+        connection.Open();
+        Console.WriteLine("Connection successful!");
+        var result = Helper.GetAllDepartment(connection);
+
+        foreach (var item in result)
+        {   
+            Console.WriteLine($"Branch Id{item.Id}, Branch Nme: {item.Name}");
+        }
+
+        connection.Close();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+    }
+}
